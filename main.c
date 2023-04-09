38a472e7c5c021343bf8ae924cb0c1d30306732a
StaZyXx <samuel.letellier@supinfo.Com>
2023-04-09 21:02:57 +0200

    continue to work on spawn player and bg player

--- main.c
+++ main.c
@@ -11,6 +11,8 @@ int maxY = 1;
 
 int amountSpawnPoint = 0;
 
+int level = 1;
+
 struct Location {
     int x;
     int y;
@@ -26,6 +28,7 @@ struct Case {
     bool hasChains;
     bool isCase;
 };
+
 
 struct Chains {
     int spawnPoints;
@@ -69,12 +72,14 @@ void game_logic();
 
 
 int main() {
-    init_level(1);
+    init_level(level);
     game_logic();
 }
 
 void init_level(int level) {
-    FILE *file = fopen("level/level1.txt", "r");
+    char filename[20];
+    sprintf(filename, "level/level%d.txt", level);
+    FILE *file = fopen(filename, "r");
 
     if (file == NULL) {
         printf("NULL");
@@ -103,6 +108,8 @@ void init_level(int level) {
             spawnPoint.spawnPoint.y = y;
             spawnPoints[amountSpawnPoint] = spawnPoint;
             amountSpawnPoint++;
+            player.location.x = x;
+            player.location.y = y;
         }
         struct Case c;
 
@@ -112,14 +119,12 @@ void init_level(int level) {
 
         c.hasChains = false;
         c.caseChar = ch;
-
         matrix[y][x] = c;
         x++;
         if (x > maxX) {
             maxX = x;
         }
     }
-
     fclose(file);
 }
 
@@ -132,14 +137,15 @@ bool is_finished() {
             if (!matrixCase.hasChains) return false;
         }
     }
-
+    level++;
     return true;
 }
 
 void game_logic() {
 
     char lettre;
-    while (!is_finished()) {
+    while (!is_finished() && level < 30) {
+        show_board();
         printf("-------------------------------------\n");
         printf("Entrez une direction (Z, Q, S, D)\n");
         printf("Annuler le coup precedent (B)\n");
@@ -157,9 +163,20 @@ void game_logic() {
         if (lettre == 'b') {
             player.location = player.lastLocation;
         }
-
-        show_board();
+        struct SpawnPoint spawnPoint;
+        if (lettre == 'c'){
+            if (amountSpawnPoint == 1){
+                printf("Nombre de chaines insuffisant\n");
+            }
+            else{
+                player.currentChains = 2;
+            }
+        }
+        if (lettre == 'x'){
+            init_level(level);
+        }
         sleep(1);
+
     }
 
 }
@@ -167,25 +184,27 @@ void game_logic() {
 bool can_move(char direction) {
     struct Case c;
     switch (direction) {
-
         case 'z':
             c = matrix[player.location.x - 1][player.location.y];
             if (player.location.x >= 0 && !c.hasChains && c.isCase) {
                 return true;
             }
             return false;
+
         case 'q':
             c = matrix[player.location.x][player.location.y - 1];
             if (player.location.y - 1 >= 0 && c.isCase) {
                 return true;
             }
             return false;
+
         case 'd':
             c = matrix[player.location.x][player.location.y + 1];
             if (player.location.y +2 < maxX && !c.hasChains && c.isCase) {
                 return true;
             }
             return false;
+
         case 's':
             c = matrix[player.location.x + 1][player.location.y];
             if (player.location.x + 2 < maxY -1 && !c.hasChains && c.isCase) {
@@ -207,7 +226,6 @@ void move(char direction) {
         case 'z':
             player.lastLocation = player.location;
             player.location.x--;
-            matrix[player.location.x][player.location.y].hasChains = true;
             break;
         case 'q':
             player.lastLocation = player.location;
@@ -230,27 +248,28 @@ int show_board() {
         for (int j = 0; j < maxX; ++j) {
             struct Case matrixCase = matrix[i][j];
 
-            if (matrixCase.hasChains) {
-                color.backgroundColor = 2;
+            if (matrixCase.hasChains == true) {
+                color.backgroundColor = 18;
                 color.textColor = -1;
                 set_color(color);
+                continue;
             }
-
             if (player.location.x == i && player.location.y == j) {
-                color.backgroundColor = 1;
+                color.backgroundColor = 18;
                 color.textColor = -1;
                 set_color(color);
                 printf("P");
                 continue;
             }
+
             if (matrixCase.caseChar == 'X') {
                 color.backgroundColor = -1;
                 color.textColor = 17;
-
                 set_color(color);
                 printf("X");
                 continue;
             }
+
             color.backgroundColor = 1;
             color.textColor = -1;
             set_color(color);
@@ -262,7 +281,6 @@ int show_board() {
 }
 
 
-// En gros, quand on commence le jeux le player doit spawn de base sur un spawn point.
 /*
  * TODO :
  * - Quand un joueur ce déplace faut checker ça chains avec le currentChains et ajouter la location dans chains et checker le montant de spawnPoints,
@@ -271,9 +289,4 @@ int show_board() {
  *
  * - Changer le boolean de hasChains
  * - Afficher un background différent (j'ai mis une couleur sur chains)
- *
- * - Faire le restart, suffit juste de faire init_level et clear le player ect...
- *
- * - Utiliser l'argument level dans init_level
- *
  */
